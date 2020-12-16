780255c580bc00a9ae6951698c03dc3c18c1e304
Renan Luiz <renanllm01@gmail.com>
2020-12-15 23:30:06 -0300

    feat: inclui alterações para solucionar desafio 02

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    handleListRepositories();
+  }, []);
+
+  async function handleListRepositories() {
+    const response = await api.get("/repositories");
+    setRepositories(response.data);
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleAddRepository() {
+    const repository = await api.post("/repositories", {
+      title: `New Repository ${repositories.length + 1}`,
+      url: "www.repository.com.br",
+      techs: ["tech1", "tech2"]
+    });
+
+    setRepositories([...repositories, repository.data]);
+
+    return (
+      <li>
+        {repository.title}
+
+        <button onClick={() => handleRemoveRepository(repository.id)}>
+          Remover
+          </button>
+      </li>
+    );
   }
+
+  async function handleRemoveRepository(idRepository) {
+    await api.delete(`/repositories/${idRepository}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== idRepository));
+  }
+
+  const renderRepositories = (repository) => {
+    return (
+      <li
+        key={repository.id}>
+        {repository.title}
+        <button onClick={() => handleRemoveRepository(repository.id)}>
+          Remover
+          </button>
+      </li>
+    )
+  };
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(renderRepositories)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
